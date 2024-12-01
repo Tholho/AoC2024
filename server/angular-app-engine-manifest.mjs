58efc2a4b6c8ec4702c1821fea8ba1b3d1ad69aa
Nick Pseudo <tomdop.dakar@gmail.com>
2024-12-01 18:47:07 +0100

    Auto-generated commit

--- server/angular-app-engine-manifest.mjs
+++
@@ -1,6 +0,0 @@
-
-export default {
-  basePath: 'https://tholho.github.io/AoC2024/',
-  entryPoints: new Map([['', () => import('./main.server.mjs')]]),
-};
-  
--- server/assets-chunks/styles-SPLAIKQF_css.mjs
+++
@@ -1,2 +0,0 @@
-export default `body{background:#0f0f23}
-`;
